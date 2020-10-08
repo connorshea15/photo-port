eada1a89168efc4919bdb2f0c203f40f08632dc1
connorshea15 <connorshea15@gmail.com>
2020-10-08 14:59:05 -0700

    Lesson 20.3.5 all category images are rendering

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -3,12 +3,14 @@ import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery({ currentCategory }) {
-  const { name, description } = currentCategory;
+  console.log("current category: " + currentCategory)
+    const { name, description } = currentCategory;
+  
   return (
     <section>
       <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
       <p>{description}</p>
-      <PhotoList  />
+      <PhotoList category={currentCategory.name}  />
     </section>
   );
 }
