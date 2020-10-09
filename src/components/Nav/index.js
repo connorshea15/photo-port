1df8979e61a25c3ce7d3e5234466a549c93d9684
connorshea15 <connorshea15@gmail.com>
2020-10-09 10:45:27 -0700

    20.4.6 conditionally render contact form

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,6 +6,8 @@ function Nav(props) {
       categories = [],
       setCurrentCategory,
       currentCategory,
+      contactSelected,
+      setContactSelected
   } = props;
 
   useEffect(() => {
@@ -29,20 +31,28 @@ function Nav(props) {
             <a
               data-testid="about"
               href="#about"
+              onClick={() => setContactSelected(false)}
             >
               About me
             </a>
           </li>
-          <li>
-            <span>Contact</span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span
+              onClick={() => {
+                setContactSelected(true)
+              }}
+              >Contact
+            </span>
           </li>
           {categories.map((category) => (
             <li className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
-                }`} key={category.name}>
+                currentCategory.name === category.name && !contactSelected && 'navActive'
+                }`} 
+                key={category.name}>
               <span
                 onClick={() => {
-                  setCurrentCategory(category)
+                  setCurrentCategory(category);
+                  setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
